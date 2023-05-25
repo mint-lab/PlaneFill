dc62653044f0d0222a208a7ad54061f1e9cf97d5
Hyunkil Hwang <83130841+Hyunkil76@users.noreply.github.com>
2023-05-25 07:40:39 +0000

    Add files via upload

---
+++ C++/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mint/Desktop/test_zed
+# Build directory: /home/mint/Desktop/test_zed/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
